2d799c7b51295641c4bf3385bbe9b63a6aa87d1a
DhirajGitHub0910 <116726913+DhirajGitHub0910@users.noreply.github.com>
2022-11-07 15:47:50 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,11 @@ def downloadgit()
 {
    git 'https://github.com/DhirajGitHub0910/maven.git'  
 }
+def building()
+{
+   sh 'mvn package'
+}
+def deployment(a)
+{
+   deploy adapters: [tomcat9(credentialsId: 'baefdbc7-9a7a-450b-b42b-d9af879cb8ea', path: '', url: 'http://44.203.164.23:8080')], contextPath: '${a}', war: '**/*.war'
+}
