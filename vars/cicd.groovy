08d0c9dd47e5e41bcf2cb8ed3a9301c38425e37b
DhirajGitHub0910 <116726913+DhirajGitHub0910@users.noreply.github.com>
2022-11-07 16:31:50 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,7 +6,7 @@ def building()
 {
    sh 'mvn package'
 }
-def deployment(a)
+def deployment(path)
 {
-   deploy adapters: [tomcat9(credentialsId: 'baefdbc7-9a7a-450b-b42b-d9af879cb8ea', path: '', url: 'http://172.31.93.121:8080')], contextPath: ${a}, war: '**/*.war'
+   deploy adapters: [tomcat9(credentialsId: 'baefdbc7-9a7a-450b-b42b-d9af879cb8ea', path: '', url: 'http://172.31.93.121:8080')], contextPath: "${path}", war: '**/*.war'
 }
